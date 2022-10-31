1c911c3f486280abe44401c5e22961c8a3e19961
giulianopoliti <117053600+giulianopoliti@users.noreply.github.com>
2022-10-31 17:07:13 -0300

    Update BATALLAS EPICAS.py

--- BATALLAS EPICAS.py
+++ BATALLAS EPICAS.py
@@ -1,3 +1,5 @@
+import random
+
 def comienzo():
     print()
     print("BATALLAS POKEMON".center(100))
@@ -16,13 +18,18 @@ def comienzo():
 
              #se retorna lo q entro el usuario para pasarlo como parametro en la proxima funcion
 
-def elegirPokemones(nombre):
-    pokemones = {
-        "Charizard": ["fuego", 120, 30],
-        "Sandlash": ["tierra", 95, 25],
-        "Tauros": ["normal", 110, 30],
-        "Dewgong": ["agua", 90, 25]
+def  elegirPokemones(nombre):
+    pokemones  = {
+        "Charizard" : [ "fuego" , 250, 30, 35, 70],
+        "Sandlash" : [ "tierra" , 280, 25, 30, 60 ],
+        "Tauros" : [ "normal" , 225, 40,25,80],
+        "Dewgong" : [ "agua" , 300, 20, 30, 50],
+        "Harrypotter" : ["mago", 180,45,40,90],
+        "Goku" : ["luchador", 210, 40,40,70],
+        "Vegetta" : ["luchador", 250, 32, 32, 70]
     }
+    for pokemon, datos in pokemones.items():
+        print ("El personaje", pokemon, "es un", datos[0], "su vida es", datos [1], "y su ataque", datos [2])
     while True:
         try:
             ent = input(f"{nombre}, ingresar nombre del pokemon: ")
@@ -31,7 +38,18 @@ def elegirPokemones(nombre):
             False
         except AssertionError:
             print(print("Pokemon no encontrado."))
-
+def habilidades():
+    print ("Ingrese 2 para ataque comun, 3 para curacion, y 3 para la definitiva")
+    decision = int(input("Que accion desea realizar?: "))
+    if decision == 2:
+        return 2
+    elif decision == 3:
+        return 3
+    elif decision == 4:
+        return 4
+    else:
+        habilidades()
+        
 def jugadorVsJugador():
     nombreJugador1 = input("Ingresar nombre del jugador 1: ")
     pokemonJugador1 = elegirPokemones(nombreJugador1)
@@ -40,25 +58,23 @@ def jugadorVsJugador():
 
     return nombreJugador1, pokemonJugador1, nombreJugador2, pokemonJugador2
 
-def partida(jugador1, jugador2, pokemon1, pokemon2):
+def partida(jugador1, pokemon1, jugador2, pokemon2):
     turno = 0
-
+    print("Elegiste a", pokemon1, "su vida es", pokemon1[1], "su ataque basico", pokemon1[2], "su curacion es", pokemon1[3], "y su definitiva", pokemon1[4])
+    print("Elegiste a", pokemon2, "su vida es", pokemon2[1], "su ataque basico", pokemon2[2], "su curacion es", pokemon2[3], "y su definitiva", pokemon2[4])
     vidaPokemon1 = pokemon1[1]
     vidaPokemon2 = pokemon2[1]
-
-    ataquePokemon1 = pokemon1[2]
-    ataquePokemon2 = pokemon2[2]
 
 
     while vidaPokemon1 > 0 and vidaPokemon2 > 0:
         print()
         if turno == 0:
-            vidaPokemon2 -= ataquePokemon1
+            vidaPokemon2 -= pokemon1[habilidades()]
             turno = 1
             print(vidaPokemon2)
 
         else:
-            vidaPokemon1 -= ataquePokemon2
+            vidaPokemon1 -= pokemon2[habilidades()]
             turno = 0
             print(vidaPokemon1)
 
@@ -70,6 +86,6 @@ def jugadorVsPc():
 modoDeJuego = comienzo()
 if modoDeJuego == 1:
     nombre1, pokemon1, nombre2, pokemon2 = jugadorVsJugador()
-    partida(nombre1, nombre2, pokemon1, pokemon2)
+    partida(nombre1, pokemon1, nombre2, pokemon2)
 else:
     jugadorVsPc()
